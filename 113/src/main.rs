1b22e89a9fc35e8b86169dc80e1295d4a36c3c7f
Łukasz Dubiel <bambucha14@gmail.com>
2019-09-02 00:16:11 +0200

    Solution on double buffer

--- 113/src/main.rs
+++ 113/src/main.rs
@@ -1,3 +1,13 @@
+fn reverse1(s: &str) -> String {
+    let mut o = String::new();
+    let splited = s.rsplit(" ");
+    splited.for_each(|word| {
+        o.push_str(word);
+        o.push(' ');
+    });
+    return o;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", reverse1("Hello world here!"));
 }
