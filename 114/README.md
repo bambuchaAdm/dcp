951cdb2e4465da36b493484f3859cb17c1b42e43
Łukasz Dubiel <bambucha14@gmail.com>
2019-09-03 01:48:01 +0200

    114 Full solution but little ugly

---
+++ 114/README.md
@@ -0,0 +1,38 @@
+Daily coding problem #113
+=========================
+
+* Difficulty: Hard
+* Ask by: Facebook
+
+Task
+----
+
+Given a string and set of delimiters, reverse the words in the string while maintaining the relative order of the delimiters.
+
+Example
+-------
+
+### Given
+
+`hello/world:here`
+
+### Then
+
+`here/world:hello`
+
+Bonus
+-----
+
+Does your solution work for:
+
+| Given | Then |
+| ----- | ---- |
+| hello/world:here/ | /here:world/hello |
+| hello//world:here | here/world/hello: |  
+
+
+### Give 1
+* "hello/world:here/"
+### Then 1
+
+* "hello//world:here"
---
+++ 114/src/main.rs
@@ -0,0 +1,37 @@
+
+fn reverseSplit(s: &str, delimiters: Vec<char>) -> String {
+    let parts = s.rsplit(|c: char| delimiters.contains(&c));
+    let mut splits = s.chars().filter(|c: &char| delimiters.contains(c));
+
+    let mut buffer: String = String::new();
+    for word in parts {
+        print!("{}", word);
+        buffer.push_str(word);
+        splits.next().map(|c: char| buffer.push(c));
+    }
+    return buffer;
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn first_example() {
+        assert_eq!(reverseSplit("hello/world:here", ['/', ':'].to_vec()), "here/world:hello")
+    }
+
+    #[test]
+    fn second_example() {
+        assert_eq!(reverseSplit("hello/world:here/",['/', ':'].to_vec()), "/here:world/hello")
+    }
+
+    #[test]
+    fn third_example() {
+        assert_eq!(reverseSplit("hello//world:here", ['/', ':'].to_vec()), "here/world/:hello")
+    }
+}
