ec0714b0dac9da54f45b2e7d0cc632753f9dfade
Łukasz Dubiel <bambucha14@gmail.com>
2019-09-03 01:47:51 +0200

    113 WIP

--- 113/src/main.rs
+++ 113/src/main.rs
@@ -1,3 +1,17 @@
+fn reverse2(s: &str) -> String {
+    let x: String = String::from(s);
+    fn inner_loop(index: usize) -> String {
+        let mut next = index + 1;
+        match x.find(' ') {
+            Some(i) =>
+              return x[index..i]
+            None =>
+              return String::new()
+        }
+    }
+    return inner_loop(0);
+}
+
 fn reverse1(s: &str) -> String {
     let mut o = String::new();
     let splited = s.rsplit(" ");
