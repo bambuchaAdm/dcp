691eb5944cbc0ce5dbc4c5b3b9be0486786ada9f
Łukasz Dubiel <bambucha14@gmail.com>
2019-09-03 02:33:54 +0200

    114 Clean build without any warning

--- 114/src/main.rs
+++ 114/src/main.rs
@@ -1,20 +1,20 @@
 
-fn reverseSplit(s: &str, delimiters: Vec<char>) -> String {
+fn reverse_split(s: &str, delimiters: Vec<char>) -> String {
     let parts = s.rsplit(|c: char| delimiters.contains(&c));
     let mut splits = s.chars().filter(|c: &char| delimiters.contains(c));
 
     let mut buffer: String = String::new();
-
     for word in parts {
-        print!("{}", word);
         buffer.push_str(word);
-        splits.next().map(|c: char| buffer.push(c));
+        for delimiter in splits.next() {
+            buffer.push(delimiter)
+        }
     }
     return buffer;
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("{}", reverse_split("Hello, world!", [',', ' '].to_vec()));
 }
 
 #[cfg(test)]
@@ -23,16 +23,16 @@ mod test {
 
     #[test]
     fn first_example() {
-        assert_eq!(reverseSplit("hello/world:here", ['/', ':'].to_vec()), "here/world:hello")
+        assert_eq!(reverse_split("hello/world:here", ['/', ':'].to_vec()), "here/world:hello")
     }
 
     #[test]
     fn second_example() {
-        assert_eq!(reverseSplit("hello/world:here/",['/', ':'].to_vec()), "/here:world/hello")
+        assert_eq!(reverse_split("hello/world:here/", ['/', ':'].to_vec()), "/here:world/hello")
     }
 
     #[test]
     fn third_example() {
-        assert_eq!(reverseSplit("hello//world:here", ['/', ':'].to_vec()), "here/world/:hello")
+        assert_eq!(reverse_split("hello//world:here", ['/', ':'].to_vec()), "here/world/:hello")
     }
 }
